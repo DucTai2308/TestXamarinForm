725a56155fc534d6ca02cc5dbd43ca1ac3521ba5
ductai <ductai@DESKTOP-3B6LJC4>
2016-06-15 09:50:01 +0700

    Add project files.

---
+++ TestCrossPlatform/TestCrossPlatform/ContentPages/StartPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestCrossPlatform.ContentPages
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowDetailsButton_Click(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DetailPage());
+        }
+        public void ShowTilteButton_Click(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TiltePage());
+        }
+        public void ShowScheduleButton_Click(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SchedulePage());
+        }
+
+    }
+}
